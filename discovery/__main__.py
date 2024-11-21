01b9b499944a0cb676225107aa55e12ede0f5fac
Kirill Birger <kbirger@gmail.com>
2024-11-21 09:25:41 -0500

    add discovery script

---
+++ discovery/__main__.py
@@ -0,0 +1,58 @@
+import itertools
+from pathlib import Path
+from smarter_client.domain.models import Command, Commands, Device, Network, User
+from smarter_client.domain.smarter_client import SmarterClient
+import json
+username = None
+password = None
+
+credentialsPath = Path('credentials').resolve()
+report_path = Path('report').resolve()
+if not credentialsPath.exists():
+    print(f"Credentials file not found at {credentialsPath}")
+    exit(1)
+    
+print(f"Reading credentials file: {credentialsPath}")
+with open('credentials', 'r') as cred:
+    try:
+        (username,password) = cred.read().splitlines()
+    except ValueError as error:
+        print(f"Credentials file is not in correct format. One line with email, followed by one line with password.")
+
+def load_from_network(client: SmarterClient, network: Network):
+    network.fetch()
+    for device in network.associated_devices:
+        device.fetch()
+        yield device
+
+client = SmarterClient()
+
+session = client.sign_in(username, password)
+
+report = {}
+
+user: User = User.from_id(client, session.local_id)
+user.fetch()
+devices = list(
+    itertools.chain.from_iterable(        
+        load_from_network(client, network)
+        for network in user.networks.values()
+    )
+)
+
+def str_cmd(cmd: Command):
+    return cmd._data
+def str_cmds(commands: Commands):
+    for command in commands.items():
+        yield {"name": command[0], "data": command[1]._data }
+report["devices"] = [{ 
+    "id": device.identifier, 
+    "status": device.status,
+    "commands": list(str_cmds(device.commands)) } for device in devices]
+
+report_str = json.dumps(report, indent=2)
+print(report_str)
+
+print(f'Writing report to {report_path}')
+with open(report_path, 'w') as out_file:
+    out_file.write(report_str)
