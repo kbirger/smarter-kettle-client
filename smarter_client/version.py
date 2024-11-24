1587abbea1b0ae5b000bba113f8618c1db8ae6e1
Kirill Birger <kbirger@gmail.com>
2024-11-24 13:16:03 -0500

    add py.typed

--- smarter_client/version.py
+++ smarter_client/version.py
@@ -4,7 +4,7 @@ from __future__ import annotations as _annotations
 
 __all__ = "VERSION", "version_info"
 
-VERSION = "0.3.0-dev.0"
+VERSION = "0.3.0-dev.1"
 """The version of Smarter Kettle Client."""
 
 
