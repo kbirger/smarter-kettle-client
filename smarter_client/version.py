8a48aa42e49a994dd3b2d8a04467654c1a05a001
Kirill Birger <kbirger@gmail.com>
2025-07-07 12:46:54 -0400

    bump download-artifact vers

--- smarter_client/version.py
+++ smarter_client/version.py
@@ -4,7 +4,7 @@ from __future__ import annotations as _annotations
 
 __all__ = "VERSION", "version_info"
 
-VERSION = "0.3.0-dev.4"
+VERSION = "0.3.0-dev.5"
 """The version of Smarter Kettle Client."""
 
 
